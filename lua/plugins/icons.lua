0cdb17193028ee864de11e9298a05cbe04635741
Ezekiel Cochran <zeke.j.cochran@gmail.com>
2025-05-15 10:31:46 -0700

    add mini.icons to icons.lua

--- lua/plugins/icons.lua
+++ lua/plugins/icons.lua
@@ -1,6 +1,14 @@
 return {
-    "nvim-tree/nvim-web-devicons",
-    config = function()
-        require("nvim-web-devicons").setup()
-    end,
+    {
+        "nvim-tree/nvim-web-devicons",
+        config = function()
+            require("nvim-web-devicons").setup()
+        end,
+    },
+    {
+        "echasnovski/mini.icons",
+        config = function()
+            require("mini.icons").setup()
+        end,
+    },
 }
