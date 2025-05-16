d4f13cf9d15caf891efd5b71b77157c2287dcd34
Ezekiel Cochran <zeke.j.cochran@gmail.com>
2025-05-16 14:10:05 -0700

    turn off lazy auto-checking for plugin updates

--- lua/ecochran/lazy.lua
+++ lua/ecochran/lazy.lua
@@ -28,9 +28,8 @@ require("lazy").setup({
         -- import your plugins
         { import = "plugins" },
     },
-    -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
     install = { colorscheme = { "habamax" } },
     -- automatically check for plugin updates
-    checker = { enabled = true },
+    checker = { enabled = false },
 })
